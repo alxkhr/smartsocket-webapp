886030608bd12e3ad4ee16413908c9bb00f0a8d7
Alexander Kehr <alex@alexanderkehr.de>
2019-10-27 09:17:48 +0100

    progressive web app

--- src/index.tsx
+++ src/index.tsx
@@ -4,3 +4,7 @@ import { render } from 'react-dom';
 import { App } from './app/app';
 
 render(<App />, document.getElementById('main'));
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js');
+}
